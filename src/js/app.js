16018eab9fbceae6356a5a91aaafa744e4cd49b4
FluxusGaming <agustint@live.com>
2020-11-28 00:19:12 -0300

    fix: navbar

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,12 @@
 const toggleNav = document.querySelector(".navbar__burger");
+const nav = document.querySelector(".navbar");
 const navMenu = document.querySelector(".navbar__menu");
 const navMenuLinks = document.querySelectorAll(".navbar__link");
+const body = document.querySelectorAll(".body");
 toggleNav.addEventListener("click", () => {
   navMenu.classList.toggle("active");
+  nav.classList.toggle("active");
+  body.classList.toggle("active");
 });
 
 navMenuLinks.forEach((element) => {
